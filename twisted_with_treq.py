8cedf4bab7b816dc57e91e0c054cee429679dffb
ProkhvatylovArtem <prokhvatylov.artem@gmail.com>
2021-10-25 01:44:22 +0300

    Updated requirements.txt (Twisted 16.6.0 instead of newer version) Changed API used for sending requests. Added writing to file with appropriate headers.

--- twisted_with_treq.py
+++ twisted_with_treq.py
@@ -1,36 +1,97 @@
 import treq
-from twisted.internet import task, reactor
-from datetime import datetime
+import csv
+import os
+
+from twisted.internet import task, reactor, _sslverify
+from twisted.web.http_headers import Headers
+
+_sslverify.platformTrust = lambda: None
 
 FILE = 'data.csv'
-URL = 'http://dataservice.accuweather.com/currentconditions/v1/'
-API_KEY = '/?apikey=OfuODmknhtgNGGBzMiNCm2yaEfAnCqxI'
-INTERVAL = 30
+URL = 'https://weatherapi-com.p.rapidapi.com/current.json?q='
+headers = {
+    'x-rapidapi-host': ['weatherapi-com.p.rapidapi.com'],
+    'x-rapidapi-key': ['4767ff11ccmsh07e3f8226920ec0p1fb674jsn4711e7bb5020']
+}
+
+INTERVAL = 30  # Time value after which the request will be repeated.
+
+
+def fileEmptyOrNotExist(file):
+    """
+    Function that check, if file we needed exist
+    :param file: str
+    :return: True if file not exist or empty.
+    """
+    return not os.path.isfile(file) or os.path.getsize(file) == 0
 
 
 def write_to_file(content):
-    with open(FILE, 'ab') as f:
-        f.write('Time of the request: {0}\n{1}\n '.format(str(datetime.now()), content))
-    print('Time of the request: {0}\n{1}\n '.format(str(datetime.now()), content))
+    """
+    Function that parse and print data into console and write it into file with appropriate headers.
+    :param content: json
+    :return: None
+    """
+    with open(FILE, 'a+') as f:
+        fieldnames = [
+            'gust_kph',
+            'last_updated',
+            'vis_miles',
+            'pressure_in',
+            'cloud',
+            'precip_mm',
+            'is_day',
+            'feelslike_c',
+            'feelslike_f',
+            'wind_mph',
+            'temp_f',
+            'temp_c',
+            'last_updated_epoch',
+            'pressure_mb',
+            'vis_km',
+            'precip_in',
+            'wind_dir',
+            'wind_kph',
+            'uv',
+            'humidity',
+            'gust_mph',
+            'wind_degree'
+        
+        ]
+        writer = csv.DictWriter(f, fieldnames=fieldnames, extrasaction='ignore')
+        print(content['current'])
+        if fileEmptyOrNotExist(FILE):
+            writer.writeheader()
+        writer.writerow(content['current'])
 
 
-def make_url(city_key):
-    return URL + str(city_key) + API_KEY
+def make_url(city):
+    """
+    Function that make unique URL for every city from basic URL and name of the city.
+    :param city: str
+    :return: URL: str
+    """
+    return URL + str(city)
 
 
 def repeating_request(cities):
-    for city in dict(cities).values():
-        d = treq.get(make_url(city))
-        d.addCallback(treq.content)
+    """
+    Function that send request and receive response from server via provided URL.
+    :param cities: str
+    :return: None
+    """
+    for city in cities:
+        d = treq.get(make_url(city), Headers(headers))
+        d.addCallback(treq.json_content)
         d.addCallback(write_to_file)
 
 
 def main():
-    cities = {
-        'Lviv': '324561',
-        'Kyiv': '324505',
-        'Kropyvnytskyi': '324291'
-    }
+    cities = [  # List of cities we want to get info about.
+        'Lviv',
+        # 'Kiev',
+        # 'Kirovohrad'
+    ]
     
     repeating = task.LoopingCall(repeating_request, cities)
     repeating.start(INTERVAL)
